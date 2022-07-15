50a30d06ea97a96ced6c40840c07bba2cc04da2d
Ben <v.skidevelopers@gmail.com>
2022-07-15 13:28:59 +0300

    fixed production.py

--- blog_test/settings/productions.py
+++ blog_test/settings/productions.py
@@ -80,13 +80,9 @@ WSGI_APPLICATION = 'blog_test.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vski_blog',
-        'USER': 'postgres',
-        'PASSWORD': 'vista90210',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR/ "db.sqlite3",
     }
 }
 
